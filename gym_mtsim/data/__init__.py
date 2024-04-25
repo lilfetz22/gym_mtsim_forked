eb0a77152653660310ef754ba77208eb66753963
lilfetz22 <billfetzner@yahoo.com>
2024-04-25 11:10:17 -0400

    new path for testing and training data

--- gym_mtsim/data/__init__.py
+++ gym_mtsim/data/__init__.py
@@ -5,6 +5,7 @@ DATA_DIR = os.path.dirname(os.path.abspath(__file__))
 
 FOREX_DATA_PATH = os.path.join(DATA_DIR, 'symbols_forex.pkl')
 FOREX_DATA_PATH_TRAIN = os.path.join(DATA_DIR, 'symbols_forex_1hr_1_1_10_4_17_24.pkl')
+FOREX_DATA_PATH_TEST = os.path.join(DATA_DIR, 'symbols_forex_1_1_24_4_11_24.pkl')
 STOCKS_DATA_PATH = os.path.join(DATA_DIR, 'symbols_stocks.pkl')
 CRYPTO_DATA_PATH = os.path.join(DATA_DIR, 'symbols_crypto.pkl')
 MIXED_DATA_PATH = os.path.join(DATA_DIR, 'symbols_mixed.pkl')
