e339a60edba47ff8521e9e392caac71efbfae149
lilfetz22 <billfetzner@yahoo.com>
2024-04-25 07:01:13 -0400

    added a model path

--- gym_mtsim/data/__init__.py
+++ gym_mtsim/data/__init__.py
@@ -4,8 +4,8 @@ import os
 DATA_DIR = os.path.dirname(os.path.abspath(__file__))
 
 FOREX_DATA_PATH = os.path.join(DATA_DIR, 'symbols_forex.pkl')
-FOREX_DATA_PATH_TRAIN = os.path.join(DATA_DIR, 'symbols_forex_1_1_20_12_31_23.pkl')
-FOREX_DATA_PATH_TEST = os.path.join(DATA_DIR, 'symbols_forex_1_1_24_4_11_24.pkl')
+FOREX_DATA_PATH_TRAIN = os.path.join(DATA_DIR, 'symbols_forex_1hr_1_1_10_4_17_24.pkl')
 STOCKS_DATA_PATH = os.path.join(DATA_DIR, 'symbols_stocks.pkl')
 CRYPTO_DATA_PATH = os.path.join(DATA_DIR, 'symbols_crypto.pkl')
 MIXED_DATA_PATH = os.path.join(DATA_DIR, 'symbols_mixed.pkl')
+MODEL_PATH = os.path.join(DATA_DIR, 'model_50K_0_testing.pkl')
