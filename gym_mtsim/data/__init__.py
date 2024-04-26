43397793615bf1ebb1c602898b2e6109cd84ab13
lilfetz22 <billfetzner@yahoo.com>
2024-04-26 07:21:42 -0400

    added the model to the data that I can use for testing

--- gym_mtsim/data/__init__.py
+++ gym_mtsim/data/__init__.py
@@ -9,4 +9,4 @@ FOREX_DATA_PATH_TEST = os.path.join(DATA_DIR, 'symbols_forex_1_1_24_4_11_24.pkl'
 STOCKS_DATA_PATH = os.path.join(DATA_DIR, 'symbols_stocks.pkl')
 CRYPTO_DATA_PATH = os.path.join(DATA_DIR, 'symbols_crypto.pkl')
 MIXED_DATA_PATH = os.path.join(DATA_DIR, 'symbols_mixed.pkl')
-MODEL_PATH = os.path.join(DATA_DIR, 'model_50K_0_testing.pkl')
+MODEL_PATH = os.path.join(DATA_DIR, 'model_0.pkl')
