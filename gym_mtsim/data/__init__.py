c31b200180aef0120440ec050da7ec477f41d6bd
lilfetz22 <billfetzner@yahoo.com>
2024-04-30 09:50:34 -0400

    added production path

--- gym_mtsim/data/__init__.py
+++ gym_mtsim/data/__init__.py
@@ -4,6 +4,7 @@ import os
 DATA_DIR = os.path.dirname(os.path.abspath(__file__))
 
 FOREX_DATA_PATH = os.path.join(DATA_DIR, 'symbols_forex.pkl')
+FOREX_DATA_PATH_PRODUCTION = os.path.join(DATA_DIR, 'symbols_forex_production.pkl')
 FOREX_DATA_PATH_TRAIN = os.path.join(DATA_DIR, 'symbols_forex_1hr_1_1_10_4_17_24.pkl')
 FOREX_DATA_PATH_TEST = os.path.join(DATA_DIR, 'symbols_forex_1_1_24_4_11_24.pkl')
 STOCKS_DATA_PATH = os.path.join(DATA_DIR, 'symbols_stocks.pkl')
