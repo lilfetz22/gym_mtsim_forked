bd12789f46a02ab0f200ffc63aed7200d1328b55
lilfetz22 <billfetzner@yahoo.com>
2024-05-20 11:37:06 +0000

    backup file

--- gym_mtsim/data/__init__.py
+++ gym_mtsim/data/__init__.py
@@ -5,6 +5,7 @@ DATA_DIR = os.path.dirname(os.path.abspath(__file__))
 
 FOREX_DATA_PATH = os.path.join(DATA_DIR, 'symbols_forex.pkl')
 FOREX_DATA_PATH_PRODUCTION = os.path.join(DATA_DIR, 'symbols_forex_production.pkl')
+FOREX_DATA_PATH_PRODUCTION_MS = os.path.join(DATA_DIR, 'symbols_forex_production_ms.pkl')
 FOREX_DATA_PATH_TRAIN = os.path.join(DATA_DIR, 'symbols_forex_1hr_1_1_10_5_3_24.pkl')
 FOREX_DATA_PATH_TEST = os.path.join(DATA_DIR, 'symbols_forex_1_1_24_4_11_24.pkl')
 STOCKS_DATA_PATH = os.path.join(DATA_DIR, 'symbols_stocks.pkl')
